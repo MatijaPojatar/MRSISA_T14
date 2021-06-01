914177292a7483757ef5f21fa6e517eb17264ebb
nadjagv <gvozdenacn@gmail.com>
2021-06-01 17:47:46 +0200

    Izveštaj o prihodima

--- backend-spring/mrsisa_backend/src/main/java/com/backend/springboot/service/IzvestajApotekaService.java
+++ backend-spring/mrsisa_backend/src/main/java/com/backend/springboot/service/IzvestajApotekaService.java
@@ -2,6 +2,7 @@ package com.backend.springboot.service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -13,10 +14,18 @@ import java.util.TreeSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.backend.springboot.domain.Lek;
+import com.backend.springboot.domain.LekUMagacinu;
+import com.backend.springboot.domain.Magacin;
 import com.backend.springboot.domain.Pregled;
 import com.backend.springboot.domain.RezervacijaLeka;
+import com.backend.springboot.domain.StavkaCenovnika;
+import com.backend.springboot.repository.LekUIzvestajuRepository;
+import com.backend.springboot.repository.LekUMagacinuRepository;
+import com.backend.springboot.repository.MagacinRepository;
 import com.backend.springboot.repository.PregledRepository;
 import com.backend.springboot.repository.RezervacijaRepository;
+import com.backend.springboot.repository.StavkaCenovnikaRepository;
 
 @Service
 public class IzvestajApotekaService {
@@ -26,6 +35,15 @@ public class IzvestajApotekaService {
 	
 	@Autowired
 	private RezervacijaRepository rezRep;
+	
+	@Autowired
+	private MagacinRepository magRep;
+	
+	@Autowired
+	private LekUMagacinuRepository lekMagRep;
+	
+	@Autowired
+	private StavkaCenovnikaRepository cenovnikRep;
 	
 	public Collection<Integer> odrzaniPreglediUIntervalu(LocalDateTime pocetak, LocalDateTime kraj, Integer apotekaId){
 		
@@ -57,35 +75,87 @@ public class IzvestajApotekaService {
 		return rezultat;
 	}
 	
-public Collection<Double> potroseniLekoviUIntervalu(LocalDate pocetak, LocalDate kraj, Integer apotekaId){
+public Collection<Double> prihodiUIntervalu(LocalDateTime pocetak, LocalDateTime kraj, Integer apotekaId){
 		
-		ArrayList<RezervacijaLeka> rezervacije = (ArrayList<RezervacijaLeka>) rezRep.findInRangeByApoteka(pocetak, kraj, apotekaId);
+		ArrayList<Pregled> pregledi = (ArrayList<Pregled>) pregledRep.findInRangeIzvrseniApoteka(pocetak, kraj, apotekaId);
+		ArrayList<RezervacijaLeka> rezervacije = (ArrayList<RezervacijaLeka>) rezRep.findInRangeByApoteka(pocetak.toLocalDate(), kraj.toLocalDate(), apotekaId);
+		
 		ArrayList<Double> rezultat = new ArrayList<Double>();
 		
-		Map<LocalDate, Double> kolicineMap = new HashMap<LocalDate, Double>();
+		Map<LocalDate, Double> prihodiMap = new HashMap<LocalDate, Double>();
 		
-		LocalDate datumPocetak = pocetak;
-		LocalDate datumKraj = kraj;
+		LocalDate datumPocetak = pocetak.toLocalDate();
+		LocalDate datumKraj = kraj.toLocalDate();
 		
 		while(datumPocetak.isBefore(datumKraj)) {
-			kolicineMap.put(datumPocetak, 0.0);
+			prihodiMap.put(datumPocetak, 0.0);
 			datumPocetak = datumPocetak.plusDays(1);
 			
 		}
 		
+
+		Magacin magacin = magRep.findOneByApotekaId(apotekaId);
+		//popraviti da se traze stavke cenovnika
 		for (RezervacijaLeka r: rezervacije) {
 			LocalDate key = r.getKreiranje();
-			Double kolicina = kolicineMap.get(key);
-			kolicineMap.put(key, kolicina + r.getKolicina());
+			Double prihodi = prihodiMap.get(key);
+			
+			Lek lek = r.getLek();
+			LekUMagacinu lekMagacin = lekMagRep.findOneByMagacinIdAndLekId(magacin.getId(), lek.getId());
+			System.out.println(lekMagacin.getId());
+			System.out.println("--------------------------------------------------------------");
+			StavkaCenovnika stavka = cenovnikRep.findOneInTimeByLekId(LocalDateTime.of(key.getYear(), key.getMonthValue(), key.getDayOfMonth(), 0, 0), lekMagacin.getId());
+			
+			if (stavka!=null) {
+				prihodiMap.put(key, prihodi + stavka.getCena() * r.getKolicina());
+			}
+			
+		}
+		for (Pregled p: pregledi) {
+			LocalDate key = p.getPocetak().toLocalDate();
+			Double prihodi = prihodiMap.get(key);
+			prihodiMap.put(key, prihodi + p.getCena());
 		}
 		
-		SortedSet<LocalDate> keys = new TreeSet<>(kolicineMap.keySet());
+		SortedSet<LocalDate> keys = new TreeSet<>(prihodiMap.keySet());
 		for (LocalDate key : keys) { 
-		   Double value = kolicineMap.get(key);
+		   Double value = prihodiMap.get(key);
 		   rezultat.add(value);
 		}
 		return rezultat;
 	}
+
+
+public Collection<Double> potroseniLekoviUIntervalu(LocalDate pocetak, LocalDate kraj, Integer apotekaId){
+	
+	ArrayList<RezervacijaLeka> rezervacije = (ArrayList<RezervacijaLeka>) rezRep.findInRangeByApoteka(pocetak, kraj, apotekaId);
+	ArrayList<Double> rezultat = new ArrayList<Double>();
+	
+	Map<LocalDate, Double> kolicineMap = new HashMap<LocalDate, Double>();
+	
+	LocalDate datumPocetak = pocetak;
+	LocalDate datumKraj = kraj;
+	
+	while(datumPocetak.isBefore(datumKraj)) {
+		kolicineMap.put(datumPocetak, 0.0);
+		datumPocetak = datumPocetak.plusDays(1);
+		
+	}
+	
+	for (RezervacijaLeka r: rezervacije) {
+		LocalDate key = r.getKreiranje();
+		Double kolicina = kolicineMap.get(key);
+		
+		kolicineMap.put(key, kolicina + r.getKolicina());
+	}
+	
+	SortedSet<LocalDate> keys = new TreeSet<>(kolicineMap.keySet());
+	for (LocalDate key : keys) { 
+	   Double value = kolicineMap.get(key);
+	   rezultat.add(value);
+	}
+	return rezultat;
+}
 	
 	
 }
