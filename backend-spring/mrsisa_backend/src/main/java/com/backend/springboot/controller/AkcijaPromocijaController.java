b97d4db8386722d7756049f88a6af0b251417133
milicajovovic <milicajovovic999@gmail.com>
2021-06-13 12:20:30 +0200

    Dodata autorizacija za neke metode

--- backend-spring/mrsisa_backend/src/main/java/com/backend/springboot/controller/AkcijaPromocijaController.java
+++ backend-spring/mrsisa_backend/src/main/java/com/backend/springboot/controller/AkcijaPromocijaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,6 +39,7 @@ public class AkcijaPromocijaController {
 	private EmailService emailService;
 	
 	@GetMapping("/pacijent/{id}")
+	@PreAuthorize("hasRole('PACIJENT')")
 	public ResponseEntity<List<AkcijaPromocijaDTO>> getAPByPacijent(@PathVariable Integer id){
 		List<AkcijaPromocija> akcijePromocije = apService.findAllByPacijent(id);
 		
