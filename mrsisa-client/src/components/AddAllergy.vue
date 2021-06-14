4467c35caabab7d455326311b2dfbae074334652
milicajovovic <milicajovovic999@gmail.com>
2021-06-14 19:14:35 +0200

    Bugfix za prikaz alergija pacijenta

--- mrsisa-client/src/components/AddAllergy.vue
+++ mrsisa-client/src/components/AddAllergy.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card>
-    <v-card-title>Dodavanje alergije: </v-card-title>
+    <v-card-title>Alergije na lekove i preparate: </v-card-title>
     <v-card-text>
       <v-form
         ref="forma"
@@ -9,60 +9,55 @@
         v-for="(lek, index) in lekovi"
         :key="index"
       >
-        <v-checkbox v-model="lek1" label="Bromazepam - Hemofarm" />
-        <v-checkbox v-model="lek2" label="Fervex - UPSA SAS" />
-        <v-checkbox v-model="lek3" label="Bactrim - Galenika" />
-        <v-checkbox v-model="lek4" label="Konjska mast - Krauterhof" />
-        <v-checkbox v-model="lek5" label="Caffetin - Alkaloid" />
+        <v-checkbox v-model="lek.alergican" :label="lek.naziv"/>
       </v-form>
     </v-card-text>
     <v-card-actions>
-      <v-btn :disabled="!valid" class="mr-4">Sačuvaj</v-btn>
-      <v-btn color="error" class="mr-4" @click="reset">Odustani</v-btn>
+      <v-btn :disabled="!valid" class="mr-4">Sačuvaj izmene</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios';
 
 export default {
   name: "AddAllergy",
 
-  props: {
-    id: Number,
+  props :{
+    pacijentId: Number,
   },
 
   data: () => ({
     lekovi: [],
-    lek1: false,
-    lek2: false,
-    lek3: true,
-    lek4: false,
-    lek5: false
+    true: true
   }),
-
-  mounted() {
-    axios.get(`http://localhost:8080/lekovi/all`).then((response) => {
-      const events = [];
-      response.data.forEach((element) => {
-        events.push({
-          naziv: element.name,
-        });
-        this.lekovi = ["Test 1"];
-      });
-    });
+  
+  async mounted() {
+    this.loadData();
   },
 
   methods: {
-      reset(){
-        this.lek1=false
-        this.lek2=false
-        this.lek3=true
-        this.lek4=false
-        this.lek5=false
-      },
+    async loadData() {
+      const lekovi = [];
+      await axios.get(`http://localhost:8080/lekovi/all`).then((response) => {
+        response.data.forEach((element) => {
+          lekovi.push({
+            id: element.id,
+            naziv: element.naziv + " - " + element.proizvodjac,
+            alergican: false,
+          });
+          this.lekovi = lekovi;
+        });
+      });
+
+      await axios.get(`http://localhost:8080/pacijent/alergije/${this.pacijentId}`).then((response) => {
+        response.data.forEach((element) => {
+          this.lekovi.find(x => x.id === element.id).alergican = true;
+        });
+      });
     },
+  }
 };
 </script>
 
