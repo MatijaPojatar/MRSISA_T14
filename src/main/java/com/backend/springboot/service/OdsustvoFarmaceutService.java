51b0ffb265eb431553b1645472211c8190cde17b
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2021-06-14 19:13:50 +0200

    Odsustvo konkurentno

--- src/main/java/com/backend/springboot/service/OdsustvoFarmaceutService.java
+++ src/main/java/com/backend/springboot/service/OdsustvoFarmaceutService.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.backend.springboot.domain.Farmaceut;
 import com.backend.springboot.domain.OdsustvoFarmaceut;
+import com.backend.springboot.domain.Savetovanje;
 import com.backend.springboot.domain.StatusZahtevaZaOdmor;
+import com.backend.springboot.dto.OdsustvoFarmaceutDTO;
 import com.backend.springboot.repository.OdsustvoFarmaceutRepository;
+import com.backend.springboot.repository.SavetovanjeRepository;
 
 @Transactional
 @Service
@@ -18,6 +22,12 @@ public class OdsustvoFarmaceutService {
 	
 	@Autowired
 	private OdsustvoFarmaceutRepository rep;
+	
+	@Autowired
+	private SavetovanjeRepository saveRep;
+	
+	@Autowired
+	private FarmaceutService farmService;
 	
 	public void save(OdsustvoFarmaceut od) {
 		rep.save(od);
@@ -51,5 +61,40 @@ public class OdsustvoFarmaceutService {
 	public List<OdsustvoFarmaceut> findAllNotOdobrenInFutureApoteka(Integer apotekaId, LocalDateTime start){
 		return rep.findAllNotOdobrenInFutureApoteka(apotekaId, start);
 	} 
+	
+	public boolean dodajOdsustvo(Integer id,OdsustvoFarmaceutDTO odsustvo) {
+		List<Savetovanje> checkList;
+		try {
+			checkList=saveRep.findInRangeForFarmaceut(id,odsustvo.getPocetak(),odsustvo.getKraj());
+		}catch(Exception e) {
+			return false;
+		}
+		int counter=0;
+		for(Savetovanje s:checkList) {
+			if(!s.isIzvrsen()) {
+				counter++;
+			}
+		}
+		if(counter!=0) {
+			return false;
+		}
+		
+		OdsustvoFarmaceut of=new OdsustvoFarmaceut();
+		Farmaceut f;
+		try {
+			f=farmService.findOne(id);
+		}catch(Exception e) {
+			return false;
+		}
+		of.setKraj(odsustvo.getKraj());
+		of.setPocetak(odsustvo.getPocetak());
+		of.setStatus(StatusZahtevaZaOdmor.OBRADA);
+		of.setFarmaceut(f);
+		of.setApoteka(f.getApoteka());
+		
+		save(of);
+		return true;
+		
+	}
 
 }
