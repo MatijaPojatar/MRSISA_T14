04c782580610087af563df30baa015160faab3c0
Natalija Sasic <sasicnatalija4@gmail.com>
2021-06-15 19:22:11 +0200

    Definisanje i izmena bodovanja za savetovanje i pregled

--- mrsisa-client/src/store/modules/loyalty.js
+++ mrsisa-client/src/store/modules/loyalty.js
@@ -4,15 +4,35 @@ import Vue from "vue";
 
 const initStanje = () => {
   return {
-    kategorije: []
+    kategorije: [],
+    bodPregled: 0,
+    bodSavetovanja: 0,
   }
 }
 const state = initStanje();
 
 const getters = {
   getAllKat: state => state.kategorije,
+  getBodPregled: state => state.bodPregled,
+  getBodSavetovanja: state => state.bodSavetovanja
 }
 const actions = {
+  async getBodPregledAction({commit}){
+    let tip = "PREGLED"
+
+    const response = await Vue.axios.get("/adminSistema/bodovanje/"+ tip+ "/"+"0");
+    console.log(response.data);
+    commit("setBodPregled", response.data.brojPoena);
+  },
+
+  async getBodSavetovanjaAction({commit}){
+    let tip = "SAVETOVANJE"
+    
+    const response = await Vue.axios.get("/adminSistema/bodovanje/"+ tip+ "/"+"0");
+    console.log(response.data);
+    commit("setbodSavetovanja", response.data.brojPoena);
+  },
+
   // eslint-disable-next-line no-unused-vars
   async addKatAction({commit}, dto){
     const response = await Vue.axios.post("/adminSistema/kategorija",dto);
@@ -24,8 +44,21 @@ const actions = {
     const response = await Vue.axios.get("/adminSistema/kategorija");
     console.log(response.data);
     commit("setKats", response.data);
+  },
+
+    // eslint-disable-next-line no-unused-vars
+  async delKatAction({commit}, id){
+    const response = await Vue.axios.delete("/adminSistema/kategorija/"+ id);
+    console.log(response.data);
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  async addStavkaAction({commit}, dto){
+    const response = await Vue.axios.post("/adminSistema/stavkaBodovanja", dto);
+    console.log(response.data);
+
   }
-  
+
 }
 const mutations = {
   // eslint-disable-next-line no-unused-vars
@@ -35,6 +68,14 @@ const mutations = {
 
   setKats(state, lista){
     state.kategorije = lista;
+  },
+
+  setBodPregled(state, br){
+    state.bodPregled = br;
+  },
+
+  setbodSavetovanja(state, br){
+    state.bodSavetovanja = br;
   }
   
 }
