912930bfb12114cd7a78f346c7213da221cb3aeb
Natalija Sasic <sasicnatalija4@gmail.com>
2021-06-15 10:23:54 +0200

    FIlter ponuda

--- mrsisa-client/src/components/Dobavljac/PregledPonuda.vue
+++ mrsisa-client/src/components/Dobavljac/PregledPonuda.vue
@@ -1,78 +1,92 @@
 <template>
+<div>
   <v-row>
-    <v-expansion-panels>
-      <v-expansion-panel
-      v-for="ponuda in ponude"
-      :key="ponuda.id"
-      @click="selekcija(ponuda)">
+    <v-card width="600">
+      <v-row align="center" justify="space-around">
+        <v-col align="center"> Status ponude: </v-col>
+        <v-col>
+          <v-checkbox v-model="prihvacena" label="Prihvaćena" />
+        </v-col>
+        <v-col>
+          <v-checkbox v-model="odbijena" label="Odbijena" />
+        </v-col>
+        <v-col>
+          <v-checkbox v-model="cekaOdg" label="Čeka na odgovor" />
+        </v-col>
+      </v-row>
+      </v-card>
 
-      <v-expansion-panel-header>
-        Ponuda za narudžbenicu: {{ponuda.narudzbenicaId}}
-      </v-expansion-panel-header>
 
-      <v-expansion-panel-content>
+      <v-expansion-panels>
+        <v-expansion-panel
+        v-for="ponuda in ponude"
+        :key="ponuda.id"
+        @click="selekcija(ponuda)">
 
-        <v-text-field
-          v-if="izmena"
-          outlined
-          v-model="cena"
-          label="Cena">
-          </v-text-field>
+        <v-expansion-panel-header>
+          Ponuda za narudžbenicu: {{ponuda.narudzbenicaId}}
+        </v-expansion-panel-header>
 
-           <v-text-field
-           v-else
-          readonly
-          outlined
-          v-model="ponuda.cena"
-          label="Cena">
-          </v-text-field>
-
-          <div 
-          v-if="izmena">
-            <v-date-picker v-model="rokDatum"></v-date-picker>
-
-          
-            <v-time-picker
-              v-model="rokVreme"
-              ampm-in-title
-              format="ampm"
-              landscape
-            ></v-time-picker>
-          </div>
+        <v-expansion-panel-content>
 
           <v-text-field
-          v-else
-          readonly
-          outlined
-          v-model="ponuda.rokIsporuke"
-          label="Rok isporuke">
-          </v-text-field>
-
-          <br/>
-            <v-btn
-            v-if="!izmena"
-            dark
-            color="blue"
-            @click="izmenaa()"
-            >
-              Izmeni
-            </v-btn>
-
-            <v-btn
             v-if="izmena"
-            dark
-            color="blue"
-            @click="uspeh()"
-            >
-              Sačuvaj izmene
-            </v-btn>
+            outlined
+            v-model="cena"
+            label="Cena">
+            </v-text-field>
 
-      </v-expansion-panel-content>
+            <v-text-field
+            v-else
+            readonly
+            outlined
+            v-model="ponuda.cena"
+            label="Cena">
+            </v-text-field>
+
+            <div 
+            v-if="izmena">
+              <v-date-picker v-model="rokDatum"></v-date-picker>
+
+            
+              <v-time-picker
+                v-model="rokVreme"
+                ampm-in-title
+                format="ampm"
+                landscape
+              ></v-time-picker>
+            </div>
+
+            <div v-else>Rok isporuke: {{ponuda.rokIsporuke | moment("dddd, MMMM Do YYYY, h:mm a")}}</div>
+
+            <div> Status ponude:  {{ponuda.status}}</div>
+            <br/>
+              <v-btn
+              v-if="!izmena"
+              dark
+              color="blue"
+              @click="izmenaa()"
+              >
+                Izmeni
+              </v-btn>
+
+              <v-btn
+              v-if="izmena"
+              dark
+              color="blue"
+              @click="uspeh()"
+              >
+                Sačuvaj izmene
+              </v-btn>
+
+        </v-expansion-panel-content>
 
 
-      </v-expansion-panel>
-    </v-expansion-panels>
-  </v-row>
+        </v-expansion-panel>
+      </v-expansion-panels>
+      
+    </v-row>
+</div>
 </template>
 
 <script>
@@ -80,12 +94,15 @@ import { mapActions, mapGetters } from "vuex";
 
 export default {
   data: () => ({
+    prihvacena: true,
+    odbijena: true,
+    cekaOdg: true,
+
     izmena: false,
     selektovanaPonuda: {},
     cena: 0.0,
     rokDatum: "2021-06-01",
     rokVreme: "10:10",
-    ponude: [{id : 5, narudzbenicaId: 3, cena: 5000, rokIsporuke: "2021-6-4 12:00"}]
   }),
   mounted () {
     this.getPonudeDobavljacaAction(this.user.id);
@@ -94,9 +111,25 @@ export default {
   computed: {
     ...mapGetters({
       user: "korisnici/getKorisnik",
-      ponude: "ponude/getPonudeDobavljaca",
+      ponudeRaw: "ponude/getPonudeDobavljaca",
       izmenaMoguca: "ponude/getIzmenaMoguca",
     }),
+
+    ponude(){
+      var filter = [];
+
+      if(this.prihvacena){
+        filter = filter.concat(this.ponudeRaw.filter(ponuda => ponuda.status == "PRIHVACENA"));
+      }
+      if(this.odbijena){
+        filter = filter.concat(this.ponudeRaw.filter(ponuda => ponuda.status==="ODBIJENA"));
+      }
+      if(this.cekaOdg){
+        filter = filter.concat(this.ponudeRaw.filter(ponuda => ponuda.status == "OBRADA"));
+      }
+
+      return filter;
+    },
   },
 
   methods: {
@@ -105,6 +138,8 @@ export default {
       izmeniPonuduAction: "ponude/izmeniPonuduAction",
       getIzmenaMogucaAction: "ponuda/getIzmenaMoguca"
     }),
+
+    
 
     selekcija(ponuda){
       this.selektovanaPonuda = ponuda;
