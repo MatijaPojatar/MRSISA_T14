b68a9d738be409d7a451b8cc2d41223ab81f785d
milicajovovic <milicajovovic999@gmail.com>
2021-06-15 20:21:24 +0200

    Dodati preporučeni lekovi u istorije termina

--- mrsisa-client/src/components/IstorijaPregleda.vue
+++ mrsisa-client/src/components/IstorijaPregleda.vue
@@ -8,12 +8,59 @@
       class="elevation-1"
     >
       <template v-slot:item.datum="{ item }">
-        <span>{{ item.start[0] + "-" + item.start[1] + "-" + item.start[2] }}</span>
+        <span>{{
+          item.start[0] + "-" + item.start[1] + "-" + item.start[2]
+        }}</span>
       </template>
       <template v-slot:item.trajanje="{ item }">
-        <span>{{ (item.end[3] * 60 + item.end[4]) - (item.start[3] * 60 + item.start[4]) }}</span>
+        <span>{{
+          item.end[3] * 60 + item.end[4] - (item.start[3] * 60 + item.start[4])
+        }}</span>
+      </template>
+      <template v-slot:item.izvestaj="{ item }">
+        <v-btn class="mx-2" dark color="blue" @click="showIzvestaj(item)">
+          Otvori
+        </v-btn>
       </template>
     </v-data-table>
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600"
+    >
+      <v-card>
+        <v-card-title class="headline">
+          Preporučeni lekovi
+        </v-card-title>
+        <template>
+          <v-simple-table dense>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">Naziv</th>
+                  <th class="text-left">Terapija</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in selectedPreporuceni" :key="item.id">
+                  <td>{{ item.naziv }}</td>
+                  <td>{{ item.terapija }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </template>
+      </v-card>
+      <v-card-actions class="justify-center">
+          <v-btn
+            class="white"
+            text
+            @click="endDialog"
+          >
+            Ok
+          </v-btn>
+        </v-card-actions>
+    </v-dialog>
   </v-card>
 </template>
 
@@ -30,8 +77,11 @@ export default {
       { text: "Datum", value: "datum" },
       { text: "Cena", value: "cena" },
       { text: "Trajanje (min)", value: "trajanje" },
+      { text: "Izveštaj", value: "izvestaj", sortable: false },
     ],
     pregledi: [],
+    selectedPreporuceni: [],
+    dialog: false,
   }),
 
   computed: {
@@ -46,10 +96,31 @@ export default {
 
   methods: {
     loadPregledi() {
-      axios.get(`/pregled/istorija/pacijent/${this.korisnik.id}`).then((response) => {
-        this.pregledi = response.data;
-      });
+      axios
+        .get(`/pregled/istorija/pacijent/${this.korisnik.id}`)
+        .then((response) => {
+          this.pregledi = response.data;
+        });
     },
+
+    showIzvestaj(item) {
+      axios.get(`/pregled/preporuceni_lekovi/${item.id}`).then((response) => {
+        const lekovi = [];
+        response.data.forEach((element) => {
+          lekovi.push({
+            terapija: element.terapija,
+            naziv: element.nazivLeka,
+            id: element.id,
+          });
+          this.selectedPreporuceni = lekovi;
+        });
+      });
+      this.dialog = true;
+    },
+
+    endDialog(){
+      this.dialog = false;
+    }
   },
 };
 </script>
--- mrsisa-client/src/components/IstorijaSavetovanja.vue
+++ mrsisa-client/src/components/IstorijaSavetovanja.vue
@@ -13,7 +13,50 @@
       <template v-slot:item.trajanje="{ item }">
         <span>{{ (item.end[3] * 60 + item.end[4]) - (item.start[3] * 60 + item.start[4]) }}</span>
       </template>
+      <template v-slot:item.izvestaj="{ item }">
+        <v-btn class="mx-2" dark color="blue" @click="showIzvestaj(item)">
+          Otvori
+        </v-btn>
+      </template>
     </v-data-table>
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600"
+    >
+      <v-card>
+        <v-card-title class="headline">
+          Preporučeni lekovi
+        </v-card-title>
+        <template>
+          <v-simple-table dense>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">Naziv</th>
+                  <th class="text-left">Terapija</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in selectedPreporuceni" :key="item.id">
+                  <td>{{ item.naziv }}</td>
+                  <td>{{ item.terapija }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </template>
+      </v-card>
+      <v-card-actions class="justify-center">
+          <v-btn
+            class="white"
+            text
+            @click="endDialog"
+          >
+            Ok
+          </v-btn>
+        </v-card-actions>
+    </v-dialog>
   </v-card>
 </template>
 
@@ -29,8 +72,11 @@ export default {
       { text: "Savetovanje", value: "name", sortable: false },
       { text: "Datum", value: "datum" },
       { text: "Trajanje (min)", value: "trajanje" },
+      { text: "Izveštaj", value: "izvestaj" },
     ],
     savetovanja: [],
+    selectedPreporuceni: [],
+    dialog: false,
   }),
 
   computed: {
@@ -49,6 +95,25 @@ export default {
         this.savetovanja = response.data;
       });
     },
+
+    showIzvestaj(item) {
+      axios.get(`/savetovanje/preporuceni_lekovi/${item.id}`).then((response) => {
+        const lekovi = [];
+        response.data.forEach((element) => {
+          lekovi.push({
+            terapija: element.terapija,
+            naziv: element.nazivLeka,
+            id: element.id,
+          });
+          this.selectedPreporuceni = lekovi;
+        });
+      });
+      this.dialog = true;
+    },
+
+    endDialog(){
+      this.dialog = false;
+    }
   },
 };
 </script>
